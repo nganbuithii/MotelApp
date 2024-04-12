15b237c6c4aba9cde042d767ed2653f30247165d
Ngân <nganbui.23112003@gmail.com>
2024-04-13 02:09:17 +0700

    xử lí con mắt ẩn/hiển password

--- src/components/common/InputPassword.js
+++ src/components/common/InputPassword.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { COLOR } from './color';
 
 const InputPassword = () => {
+    
     const [text, setText] = React.useState('');
-
+    // set trạng thái hiện pass = false là ẩn
+    const [isPasswordVisible, setPasswordVisible] = React.useState(false);
     return (
         <TextInput 
             style={[styles.input]}
             label="Mật khẩu"
             selectionColor={COLOR.PRIMARY}
-            secureTextEntry
-            right={<TextInput.Icon icon="eye" />}
+            // prop secure là ẩn pass nếu prop là true(ẩn) -> is pass là true
+            secureTextEntry={!isPasswordVisible}
+            right={<TextInput.Icon icon={isPasswordVisible ? 'eye' : 'eye-off'}
+            onPress={() => setPasswordVisible(!isPasswordVisible)} />}
+
             theme={{ colors: { primary: COLOR.PRIMARY}}} // Thiết lập màu chính cho TextInput
         />
     );
