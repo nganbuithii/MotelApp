31bddc9efc5c344403cdf37569e3a07bea5ce1c2
Ngân <nganbui.23112003@gmail.com>
2024-04-14 21:43:47 +0700

    fix up avatar

--- src/components/Auth/UploadImg.js
+++ src/components/Auth/UploadImg.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Image, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
-import ButtonAuth from '../common/ButtonAuth';
-import MyStyles from '../../Styles/MyStyles';
-import { COLOR } from '../common/color';
+import React, { useState, useEffect } from "react";
+import { Image, View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import ButtonAuth from "../common/ButtonAuth";
+import MyStyles from "../../Styles/MyStyles";
+import { COLOR } from "../common/color";
+import defaultImage from "../../assets/images/avt.gif"; // Import ảnh mặc định
 
 const UploadImg = () => {
     const [image, setImage] = useState(null);
-    const [defaultImage, setDefaultImage] = useState(require('../../assets/images/avt.gif'));
+    const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
+
+    const checkForCameraRollPermission = async () => {
+        const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+            alert(
+                "Please grant camera roll permissions inside your system's settings"
+            );
+        } else {
+            console.log("Media Permissions are granted");
+        }
+    };
 
     useEffect(() => {
-        // Kiểm tra quyền truy cập thư viện ảnh khi component được render lần đầu tiên
-        (async () => {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (status !== 'granted') {
-                alert('Quyền truy cập thư viện ảnh bị từ chối!');
-            }
-        })();
-    }, []); // Mảng phụ thuộc rỗng đảm bảo useEffect chỉ chạy một lần khi component được render lần đầu tiên
+        checkForCameraRollPermission();
+    }, []);
 
     const addImage = async () => {
         let _image = await ImagePicker.launchImageLibraryAsync({
@@ -29,7 +35,8 @@ const UploadImg = () => {
         });
         console.log(JSON.stringify(_image));
         if (!_image.cancelled) {
-            setImage(_image.uri);
+            setImage(_image.assets[0].uri);
+            setUploadButtonText("Change Image");
         }
     };
 
@@ -37,11 +44,18 @@ const UploadImg = () => {
         <View style={MyStyles.container}>
             <Text style={MyStyles.textHead}> Chọn ảnh đại diện</Text>
             <View style={imageUploaderStyles.container}>
-                {!image && <Image source={defaultImage} style={{ width: 200, height: 200 }} />}
-                {image && <Image source={{ uri: image }} style={{ flex: 1, aspectRatio: 4/3 }} />}
+                {image ? (
+                    <Image source={{ uri: image }} style={{ flex: 1, aspectRatio: 1 }} />
+                ) : (
+                    <Image source={defaultImage} style={{ width: 200, height: 200 }} /> // Sử dụng biến defaultImage
+                )}
+
                 <View style={imageUploaderStyles.uploadBtnContainer}>
-                    <TouchableOpacity onPress={addImage} style={imageUploaderStyles.uploadBtn}>
-                        <Text>{image ? 'Edit' : 'Upload'} Image</Text>
+                    <TouchableOpacity
+                        onPress={addImage}
+                        style={imageUploaderStyles.uploadBtn}
+                    >
+                        <Text>{uploadButtonText}</Text>
                         <AntDesign name="camera" size={20} color="black" />
                     </TouchableOpacity>
                 </View>
@@ -56,26 +70,26 @@ const imageUploaderStyles = StyleSheet.create({
         elevation: 2,
         height: 200,
         width: 200,
-        backgroundColor: '#efefef',
-        position: 'relative',
+        backgroundColor: "#efefef",
+        position: "relative",
         borderRadius: 999,
-        overflow: 'hidden',
-        marginBottom:20,
-        marginTop:20
+        overflow: "hidden",
+        marginBottom: 20,
+        marginTop: 20,
     },
     uploadBtnContainer: {
         opacity: 0.7,
-        position: 'absolute',
+        position: "absolute",
         right: 0,
         bottom: 0,
         backgroundColor: COLOR.gray,
-        width: '100%',
-        height: '50%',
+        width: "100%",
+        height: "50%",
     },
     uploadBtn: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
 
